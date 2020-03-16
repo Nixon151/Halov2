94c622d18e3803856621fc707c031dced89cb502
Nixon151 <2020heroz12@gmail.com>
2020-03-16 17:05:47 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -14,7 +14,7 @@ var l_serverImage = "";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "CrimsonRoleplay";
+var l_serverName = "Blacklight Servers";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
